85934efe9821d21251e2684824505fad0496f6de
raihan-biswas-dev <raihanbiswas.dev@gmail.com>
2022-03-12 16:58:07 +0600

    JS Digital Clock

---
+++ js/script.js
@@ -0,0 +1,47 @@
+// Stop Watch Start 
+
+function digitalClock() {
+      let date = new Date();
+      let hours = date.getHours();
+      let minutes = date.getMinutes();
+      let seconds = date.getSeconds();
+      let timeFormat = "AM";
+
+
+      if (hours == 0) {
+            hours = 12;
+      }
+      if (hours > 12) {
+            hours = hours - 12;
+            timeFormat = "PM";
+      }
+
+
+      // if (hours < 10) {
+      //       hours = "0" + hours;
+      // }
+
+      hours = hours < 10 ? "0" + hours : hours;
+      minutes = minutes < 10 ? "0" + minutes : minutes;
+      seconds = seconds < 10 ? "0" + seconds : seconds;
+
+
+      let finalTime = `${hours} : ${minutes} : ${seconds}`;
+
+      document.querySelector(".time").innerText = finalTime;
+      document.querySelector("ampm").innerText = timeFormat;
+
+      setInterval(() => {
+            digitalClock();
+      }, 1000)
+}
+
+
+digitalClock();
+
+
+
+
+
+
+// Stop Watch End
